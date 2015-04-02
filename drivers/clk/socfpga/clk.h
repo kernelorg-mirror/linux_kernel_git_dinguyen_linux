92b449f2de5b0196429d08e2b99ccfbf6182042c
Dinh Nguyen <dinguyen@opensource.altera.com>
2015-04-02 11:35:08 -0500

    clk: socfpga: add a clock driver for the Arria 10 platform
    The clocks on the Arria 10 platform is a bit different than the Cyclone/Arria 5
    platform that it should just have it's own driver.
    
    Signed-off-by: Dinh Nguyen <dinguyen@opensource.altera.com>
    ---
    v3: Assign pointer to NULL instead of integer 0 per sparse check

--- drivers/clk/socfpga/clk.h
+++ drivers/clk/socfpga/clk.h
@@ -34,10 +34,14 @@
 	((((smplsel) & 0x7) << 3) | (((drvsel) & 0x7) << 0))
 
 extern void __iomem *clk_mgr_base_addr;
+extern void __iomem *clk_mgr_a10_base_addr;
 
 void __init socfpga_pll_init(struct device_node *node);
 void __init socfpga_periph_init(struct device_node *node);
 void __init socfpga_gate_init(struct device_node *node);
+void __init socfpga_a10_pll_init(struct device_node *node);
+void __init socfpga_a10_periph_init(struct device_node *node);
+void __init socfpga_a10_gate_init(struct device_node *node);
 
 struct socfpga_pll {
 	struct clk_gate	hw;
