7dc2077fe6e1e18f7d874e9a0a9eaae081b8d32c
Kah Jing Lee <kah.jing.lee@intel.com>
2024-02-01 17:32:22 +0800

    drivers: firmware: stratix10-rsu: Query device info
    Extend Intel Remote System Update (RSU) driver to get device info table
    using SMC call. Device info table contains size and erasesize for multiple
    flashes. Empty flash will return 0 for size and erasesize.
    
    Signed-off-by: Kah Jing Lee <kah.jing.lee@intel.com>
    Signed-off-by: Dinh Nguyen <dinguyen@kernel.org>

--- include/linux/firmware/intel/stratix10-svc-client.h
+++ include/linux/firmware/intel/stratix10-svc-client.h
@@ -124,6 +124,9 @@ struct stratix10_svc_chan;
  * @COMMAND_RSU_DCMF_STATUS: query firmware for the DCMF status
  * return status is SVC_STATUS_OK or SVC_STATUS_ERROR
  *
+ * @COMMAND_RSU_GET_DEVICE_INFO: query firmware for QSPI device info
+ * return status is SVC_STATUS_OK or SVC_STATUS_ERROR
+ *
  * @COMMAND_FCS_REQUEST_SERVICE: request validation of image from firmware,
  * return status is SVC_STATUS_OK, SVC_STATUS_INVALID_PARAM
  *
@@ -158,6 +161,7 @@ enum stratix10_svc_command_code {
 	COMMAND_RSU_DCMF_VERSION,
 	COMMAND_RSU_DCMF_STATUS,
 	COMMAND_FIRMWARE_VERSION,
+	COMMAND_RSU_GET_DEVICE_INFO,
 	/* for FCS */
 	COMMAND_FCS_REQUEST_SERVICE = 20,
 	COMMAND_FCS_SEND_CERTIFICATE,
